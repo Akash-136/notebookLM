a06eb762bdfb8894f7733dbfdfa77f7c180e21ed
Akash <akashdhaliwal136@gmail.com>
2026-04-25 22:59:36 +0530

    small changes

--- backend/src/controllers/users/login.js
+++ backend/src/controllers/users/login.js
@@ -5,25 +5,6 @@ import User from "../../models/users/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const registerUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = new User({ name, email, password:hashedPassword });
-
-    await user.save();
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
 
 export const loginUser = async (req, res) => {
     try {
@@ -46,13 +27,4 @@ export const loginUser = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 
-};
-
-export const getUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
